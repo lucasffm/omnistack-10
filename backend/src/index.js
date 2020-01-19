5e963e5d1d58e4cfa0442ae498e86c361fd07fa9
Lucas de Freitas <lucasffm@gmail.com>
2020-01-18 21:34:24 -0300

    added cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
+import cors from "cors";
 
 import routes from "./routes";
 
@@ -12,6 +13,7 @@ mongoose.connect(
     useUnifiedTopology: true
   }
 );
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.listen(3000, () => {
